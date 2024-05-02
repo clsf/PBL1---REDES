a56476acdc312c2be93e0d45af79a727cfbd4fe5
Cláudia <96408566+clsf@users.noreply.github.com>
2024-05-02 13:03:37 -0300

    Update CommunicationWithApplication.py

--- broker/CommunicationWithApplication.py
+++ broker/CommunicationWithApplication.py
@@ -4,7 +4,7 @@ from Communication import communication
 app = Flask(__name__)
 
 def start_flask_server():
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=5000)
 
 @app.route("/test", methods=["GET"])
 def testIntegration ():
